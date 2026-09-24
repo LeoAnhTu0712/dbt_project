e5c9299272fefa98a46db846047b209101e3f14f
leoanhtu0712 <leoanhtu071200@gmail.com>
2026-09-24 15:06:56 +0700

    msg

--- models/analytics/supplier.sql
+++ models/analytics/supplier.sql
@@ -1,3 +1,3 @@
-select *, UPPER(supplier_id) as upper_supplier_id
+select *, UPPER(supplier_id) as upper_supplier_id -- mới thêm vàogit push origin main
  from `bustling-bot-508302-n4.Test.supplier_clone`
  order by 1
